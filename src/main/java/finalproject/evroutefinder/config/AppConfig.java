6546aa4d5c82201cf27c02ef45fcb389a4ed0a64
Olivier Laborde <laborde.olivier@gmail.com>
2021-02-23 12:52:20 +0200

    changed app .url

--- src/main/java/finalproject/evroutefinder/config/AppConfig.java
+++ src/main/java/finalproject/evroutefinder/config/AppConfig.java
@@ -1,7 +1,6 @@
 package finalproject.evroutefinder.config;
 
 import org.springframework.beans.factory.annotation.Value;
-
 import org.springframework.context.annotation.Configuration;
 
 
---
+++ src/main/java/finalproject/evroutefinder/config/MailgunConfig.java
@@ -0,0 +1,7 @@
+package finalproject.evroutefinder.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MailgunConfig {
+}
