8edacfdd75495b87bb04f73b016f5292b091a09f
Olivier Laborde <laborde.olivier@gmail.com>
2021-02-27 17:30:55 +0200

    added domain to allowed origins

--- src/main/java/finalproject/evroutefinder/config/WebConfig.java
+++ src/main/java/finalproject/evroutefinder/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry corsRegistry) {
         corsRegistry.addMapping("/**")
                 .allowedOrigins("http://localhost:4200", "http://ev-route-finder.herokuapp.com",
-                                "https://ev-route-finder.herokuapp.com", "http://ev-route-finder.herokuapp.com")
+                                "https://ev-route-finder.olivier-laborde.com", "http://ev-route-finder.olivier-laborde.com")
                 .allowedMethods("*")
                 .maxAge(3600L)
                 .allowedHeaders("*")
