da82792613f143f37cd9a0a6d7a1793bee8319f4
Olivier Laborde <laborde.olivier@gmail.com>
2021-03-03 10:27:55 +0200

    1.0 version

--- src/main/java/finalproject/evroutefinder/repository/RefreshTokenRepository.java
+++ src/main/java/finalproject/evroutefinder/repository/RefreshTokenRepository.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@Repository
+
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
     Optional<RefreshToken> findByToken(String token);
     void deleteByToken(String token);
